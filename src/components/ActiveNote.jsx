4ac59830b8b54bd1ed27fff715f6553d9fd31a69
davinmetoti <110443558+DavinMetoti@users.noreply.github.com>
2023-09-22 10:58:27 +0700

    first commit

--- src/components/ActiveNote.jsx
+++ src/components/ActiveNote.jsx
@@ -1,29 +1,18 @@
+import React from "react";
+import NoteCard from './NoteCard';
+
 function ActiveNode({ onDelete, onActive, notes }) {
     return (
         <div className="container-list">
             <div className="list">
-                {notes.map((note) => (
-                    <NoteCard
-                        changeButton="Arsip"
-                        key={note.id}
-                        onDelete={onDelete}
-                        buttonData={onActive}
-                        {...note}
-                    />
-                ))}
+                {
+                    notes.map(note => (
+                        <NoteCard changeButton="Arsip" key={note.id} onDelete={onDelete} buttonData={onActive} {...note} />
+                    ))
+                }
             </div>
         </div>
-    );
+    )
 }
-
-ActiveNode.propTypes = {
-    onDelete: PropTypes.func.isRequired,
-    onActive: PropTypes.func.isRequired,
-    notes: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-        })
-    ).isRequired,
-};
 
 export default ActiveNode;
